06e73b1340c1436b4aa8b7fc6400bacb3a699007
Усачев Владислав <rekrut313@gmail.com>
2023-03-17 10:14:05 +0300

    Конце моего рефакоринга

--- refactofing.spec.ts
+++ refactofing.spec.ts
@@ -1,5 +1,55 @@
+import { uid } from 'uid';
+
+describe('Test refactoring', () => {
+    it('Final result is the same', () => {
+        const productBasePrice = 300;
+        const productDiscountRate = 0.1;
+        const productDiscountThreshold = 7;
+        const productsQuantity = 8;
+        const shippingDiscountThreshold = 1000;
+        const shippingDiscountFeePerCase = 50;
+        const shippingFeePerCase = 70;
+
+        const priceViaBadMethod = priceOrder(
+            {
+                basePrice: productBasePrice,
+                discountRate: productDiscountRate,
+                discountThreshold: productDiscountThreshold,
+            },
+            productsQuantity,
+            {
+                discountThreshold: shippingDiscountThreshold,
+                discountFee: shippingDiscountFeePerCase,
+                feePerCase: shippingFeePerCase,
+            }
+        );
+
+        const productDiscountCalculator = new ProductDiscountCalculatorViaQuantity(
+            productDiscountThreshold,
+            productDiscountRate
+        );
+
+        const shippingCostCalculator = new ShippingCostCalculatorViaAllProductsCost(
+            shippingDiscountThreshold,
+            shippingDiscountFeePerCase,
+            shippingFeePerCase
+        );
+
+        const orderCalculator = new OrderCalculator(
+            productDiscountCalculator,
+            shippingCostCalculator
+        );
+
+        const product = new Product('Chair', productBasePrice);
+        orderCalculator.addProductToCart(product, productsQuantity);
+        const priceViaMyMethod = orderCalculator.calculateOrder();
+
+        expect(priceViaBadMethod).toBe(priceViaMyMethod);
+    });
+});
+
 // Bad Code
-function badPriceOrder(
+function priceOrder(
     product: {
         basePrice: number;
         discountRate: number;
@@ -10,22 +60,26 @@ function badPriceOrder(
         discountThreshold: number;
         discountFee: number;
         feePerCase: number;
-    },
+    }
 ) {
     const basePrice = product.basePrice * quantity;
-    
-    const discount = Math.max(quantity - product.discountThreshold, 0)
-        * product.basePrice * product.discountRate;
 
-    const shippingPerCase = basePrice > shippingMethod.discountThreshold
-        ? shippingMethod.discountFee : shippingMethod.feePerCase;
-    
+    const discount =
+        Math.max(quantity - product.discountThreshold, 0) *
+        product.basePrice *
+        product.discountRate;
+
+    const shippingPerCase =
+        basePrice > shippingMethod.discountThreshold
+            ? shippingMethod.discountFee
+            : shippingMethod.feePerCase;
+
     const shippingCost = quantity * shippingPerCase;
 
     return basePrice - discount + shippingCost;
 }
 
-// Good Code
+// My Code
 abstract class AbstractProductDiscountCalculator {
     discountThreshold: number;
     discountRate: number;
@@ -49,7 +103,7 @@ abstract class AbstractShippingCostCalculator {
         this.feePerCase = feePerCase;
     }
 
-    abstract calculateCost(allProductsCost: number): number;
+    abstract calculateCost(allProductsCost: number, quantity: number): number;
 }
 
 class ProductDiscountCalculatorViaQuantity extends AbstractProductDiscountCalculator {
@@ -58,8 +112,9 @@ class ProductDiscountCalculatorViaQuantity extends AbstractProductDiscountCalcul
     }
 
     calculateDiscount(baseProductPrice: number, quantity: number): number {
-        return Math.max(quantity - this.discountThreshold, 0)
-            * baseProductPrice * this.discountRate;
+        return (
+            Math.max(quantity - this.discountThreshold, 0) * baseProductPrice * this.discountRate
+        );
     }
 }
 
@@ -68,57 +123,69 @@ class ShippingCostCalculatorViaAllProductsCost extends AbstractShippingCostCalcu
         super(discountThreshold, discountFeePerCase, feePerCase);
     }
 
-    calculateCost(allProductsCost: number): number {
+    calculateCost(allProductsCost: number, quantity: number): number {
         return allProductsCost > this.discountThreshold
-            ? this.discountFeePerCase : this.feePerCase;
+            ? this.discountFeePerCase * quantity
+            : this.feePerCase * quantity;
     }
 }
 
 class Product {
+    id: string;
     name: string;
     basePrice: number;
-    discountCalculator: AbstractProductDiscountCalculator; 
 
-    constructor(name: string, basePrise: number, discountCalculator: AbstractProductDiscountCalculator) {
+    constructor(name: string, basePrise: number) {
+        this.id = uid();
         this.name = name;
         this.basePrice = basePrise;
-        this.discountCalculator = discountCalculator;
     }
-}
-
-interface IShoppingCartItem {
-    productInfo: Product;
-    quantity: number;
 }
 
 class OrderCalculator {
-    cart: IShoppingCartItem[]
-    discountCalculator: AbstractProductDiscountCalculator;
+    private _cart: { productInfo: Product; quantity: number }[] = [];
+    productDiscountCalculator: AbstractProductDiscountCalculator;
     shippingCostCalculator: AbstractShippingCostCalculator;
 
     constructor(
-        cart: IShoppingCartItem[],
         discountCalculator: AbstractProductDiscountCalculator,
-        shippingCostCalculator: AbstractShippingCostCalculator,
+        shippingCostCalculator: AbstractShippingCostCalculator
     ) {
-        this.cart = cart;
-        this.discountCalculator = discountCalculator;
+        this.productDiscountCalculator = discountCalculator;
         this.shippingCostCalculator = shippingCostCalculator;
     }
 
-    calculateOrder() {
-        return this.cart.reduce((acc, product) => {
-            const allProductsCost = product.productInfo.basePrice * product.quantity;
-            const discount = this.discountCalculator.calculateDiscount(product.productInfo.basePrice, product.quantity);
-            const shippingCost = this.shippingCostCalculator.calculateCost(allProductsCost);
+    addProductToCart(product: Product, quantity?: number) {
+        const isAlreadyInCart = !!this._cart.find((item) => item.productInfo.id === product.id);
 
-            return allProductsCost - discount + shippingCost;
-        }, 0)
+        if (isAlreadyInCart) {
+            this._cart = this._cart.map((item) => {
+                if (item.productInfo.id === product.id) {
+                    return { ...item, quantity: (item.quantity += quantity ?? 1) };
+                }
+
+                return item;
+            });
+        } else {
+            this._cart.push({ productInfo: product, quantity: quantity ?? 1 });
+        }
+    }
+
+    calculateOrder() {
+        return this._cart.reduce((acc, product) => {
+            const allProductsCost = product.productInfo.basePrice * product.quantity;
+
+            const discount = this.productDiscountCalculator.calculateDiscount(
+                product.productInfo.basePrice,
+                product.quantity
+            );
+
+            const shippingCost = this.shippingCostCalculator.calculateCost(
+                allProductsCost,
+                product.quantity
+            );
+
+            return acc + allProductsCost - discount + shippingCost;
+        }, 0);
     }
 }
-
-const productDiscountCalculator = new ProductDiscountCalculatorViaQuantity(5, 0.1);
-const product = new Product('Chair', 500, productDiscountCalculator);
-const shippingCostCalculator = new ShippingCostCalculatorViaAllProductsCost(1000, 50, 100);
-const orderCalculator = new OrderCalculator()
-priceOrder(product, 7, shippingCostCalculator)
